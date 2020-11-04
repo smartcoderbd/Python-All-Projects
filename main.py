63de8e736c5ba6cc8ef9f3234069b3be95a220b2
smartcoderbd <69118452+smartcoderbd@users.noreply.github.com>
2020-11-05 04:20:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import nltk
 from newspaper import Article
-url="https://www.thedailystar.net/country/news/flight-saudi-arabia-few-hours-still-waiting-covid-19-certificate-1967405"
+url="put-your-article-link"
 article = Article(url)
 article.download()
 article.parse()
